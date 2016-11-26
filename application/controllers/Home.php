48f23edf3afd95a8f1a7603e9266b25bc0055f9f
orathaiHaru <nachin.orathai@hotmail.com>
2016-11-26 14:42:59 +0700

    เพิ่ม htaccess

---
+++ application/controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:41
+ */
+class Home extends CI_Controller
+
+{
+    public function index()
+    {
+        echo "hello";
+    }
+
+
+
+
+
+
+
+}
