7d30169d4fc1763ae97545c0129c4ee83ace7257
orathaiHaru <nachin.orathai@hotmail.com>
2016-11-26 16:30:46 +0700

    footer นะ

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,10 +11,18 @@ class Home extends CI_Controller
 {
     public function index()
     {
+
+        $this->load->view('layout/header_view');
         $this->load->view('home_view');
+        $this->load->view('layout/footer_view');
+
     }
 
+    public function  test() {
+        $this->load->view('layout/header_view');
+        $this->load->view('layout/test_view');
 
+    }
 
 
 
---
+++ application/views/layout/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi architecto deserunt dolore doloribus eaque, earum
+    eius eos expedita facilis magnam nostrum officia placeat quia, quidem repellendus sapiente ut vel vero.
+</div>
+<div>Architecto asperiores aut id laboriosam modi obcaecati odio quas quos repellat unde. Animi aspernatur eligendi
+    eveniet expedita facere, hic libero maiores molestias nam necessitatibus non odit quidem soluta temporibus
+    voluptate.
+</div>
+<div>Culpa, officia, vel? A accusantium aliquam at consequatur, dignissimos dolore dolores esse eum ipsa libero maiores
+    molestias neque nihil nobis pariatur quae qui quo quos, tenetur ullam unde voluptate voluptatum.
+</div>
